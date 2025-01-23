f4b7251cb8c727247feaae00c61e2f4796619ace
rars2805 <richardant282003@gmail.com>
2025-01-22 20:58:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
 import tkinter as tk
 from tkinter import *
 import customtkinter as ctk
-from PIL import ImageTk, Image
-
-# Variables globales
-player_score = 0
-computer_score = 0
-empate_score = 0
+from forms.design_master_form import design_master_form
+from PIL import Image, ImageTk
 
 # Configuración de la apariencia de CustomTkinter
 ctk.set_appearance_mode("dark")  # Opciones: "light", "dark", "system"
-ctk.set_default_color_theme("blue")  
+ctk.set_default_color_theme("blue")  # Cambia el tema de color
+
+class SelectDifficulty(tk.Toplevel):
+    def __init__(self, parent, on_difficulty_selected):
+        super().__init__(parent)
+        self.geometry("300x200")
+        self.title("Seleccionar Dificultad")
+
+        label = tk.Label(self, text="Selecciona la Dificultad", font=("Helvetica", 12))
+        label.pack(pady=10)
+
+        self.on_difficulty_selected = on_difficulty_selected
+
+        easy_button = tk.Button(self, text="Facil", command=lambda: self.set_difficulty("easy"))
+        easy_button.pack(pady=5)
+        medium_button = tk.Button(self, text="Medio", command=lambda: self.set_difficulty("medium"))
+        medium_button.pack(pady=5)
+        hard_button = tk.Button(self, text="Dificil", command=lambda: self.set_difficulty("hard"))
+        hard_button.pack(pady=5)
+
+    def set_difficulty(self, selected_difficulty):
+        self.on_difficulty_selected(selected_difficulty)
+        self.destroy()
+        
+class SelectSymbol(tk.Toplevel):
+    def __init__(self, parent, difficulty, on_symbol_selected):
+        super().__init__(parent)
+        self.geometry("300x200")
+        self.title("Seleccionar Ficha")
+
+        label = tk.Label(self, text="Selecciona tu Ficha", font=("Helvetica", 12))
+        label.pack(pady=10)
+
+        self.difficulty = difficulty
+        self.on_symbol_selected = on_symbol_selected
+
+        if difficulty == "medium":
+            tk.Button(self, text="Y", command=lambda: self.close_game("Y")).pack(pady=5)
+            tk.Button(self, text="Z", command=lambda: self.close_game("Z")).pack(pady=5)
+        elif difficulty == "hard":
+            tk.Button(self, text="S", command=lambda: self.close_game("S")).pack(pady=5)
+            tk.Button(self, text="M", command=lambda: self.close_game("M")).pack(pady=5)
+        else:
+            tk.Button(self, text="X", command=lambda: self.close_game("X")).pack(pady=5)
+            tk.Button(self, text="O", command=lambda: self.close_game("O")).pack(pady=5)
+
+    def close_game(self, symbol):
+        self.on_symbol_selected(symbol)
+        
 
 class MainMenu(ctk.CTk):
     def __init__(self):
@@ -20,14 +64,21 @@ class MainMenu(ctk.CTk):
         self.geometry("1080x720")
 
         # Cargar la imagen de fondo del menú
-        self.original_image = Image.open("gif1.gif")  
+        try:
+            self.original_image = Image.open("./images/gif1.gif")
+        except Exception as e:
+            print(f"Error al cargar la imagen: {e}")
+            self.original_image = None  # O asigna una imagen por defecto
+
         self.background_label = tk.Label(self)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)  
 
+        # Título
         self.title_label = ctk.CTkLabel(self, text="CHOCLO GAME", font=("Arial", 70), text_color="white")
         self.title_label.pack(pady=(100, 20))
 
-        self.play_button = ctk.CTkButton(self, text="JUGAR", command=self.open_start_window, width=300, height=80, fg_color="#0b5345", hover_color="lightgreen", font=("Arial", 20))
+        # Botones
+        self.play_button = ctk.CTkButton(self, text="JUGAR", command=self.play, width=300, height=80, fg_color="#0b5345", hover_color="lightgreen", font=("Arial", 20))
         self.play_button.pack(pady=20)
 
         self.options_button = ctk.CTkButton(self, text="OPCIONES", command=self.open_options, width=300, height=80, fg_color="#0b5345", hover_color="lightgreen", font=("Arial", 20))
@@ -36,233 +87,60 @@ class MainMenu(ctk.CTk):
         self.quit_button = ctk.CTkButton(self, text="SALIR", command=self.quit, width=300, height=80, fg_color="#0b5345", hover_color="lightgreen", font=("Arial", 20))
         self.quit_button.pack(pady=20)
 
+        # Redimensionar la imagen al tamaño de la ventana
         self.bind("<Configure>", self.resize_background)
 
         self.update_background()
 
+    def update_background(self):
+        if self.original_image is not None:
+            width = self.winfo_width()
+            height = self.winfo_height()
+
+            resized_image = self.original_image.resize((width, height ), Image.LANCZOS)
+            self.background_photo = ImageTk.PhotoImage(resized_image)
+
+            self.background_label.configure(image=self.background_photo)
+            self.background_label.image = self.background_photo  # Mantener una referencia
+        else:
+            print("No se pudo cargar la imagen de fondo.")  # Mensaje de error si la imagen no se carga
+
+
     def resize_background(self, event):
         self.update_background()
 
-    def update_background(self):
-        width = self.winfo_width()
-        height = self.winfo_height()
+    def play(self):
+        self.withdraw()  # Oculta el menú principal
+        self.select_difficulty()
 
-        resized_image = self.original_image.resize((width, height), Image.LANCZOS)
-        self.background_photo = ImageTk.PhotoImage(resized_image)
+    def select_difficulty(self):
+        SelectDifficulty(self, self.on_difficulty_selected)
 
-        self.background_label.configure(image=self.background_photo)
-        self.background_label.image = self.background_photo 
+    def on_difficulty_selected(self, difficulty):
+        self.difficulty = difficulty
+        self.select_symbol()
 
-    def open_start_window(self):
-        self.withdraw() 
-        start_window() 
+    def select_symbol(self):
+        SelectSymbol(self, self.difficulty, self.on_symbol_selected)
+
+    def on_symbol_selected(self, symbol):
+        self.player_symbol = symbol
+        if self.difficulty == "easy":
+            self.computer_symbol = "O" if symbol == "X" else "X"
+        elif self.difficulty == "medium":
+            self.computer_symbol = "Y" if symbol == "Z" else "Z"
+        elif self.difficulty == "hard":
+            self.computer_symbol = "M" if symbol == "S" else "S"
+        self.start_game()
+
+    def start_game(self):
+        self.destroy()
+        game_window = design_master_form(self.difficulty, self.player_symbol, self.computer_symbol)
+        game_window.mainloop()
 
     def open_options(self):
-        self.withdraw()  
-        options_window = OptionsWindow(self)  
-
-def start_window():
-    global start_window_instance
-    start_window_instance = tk.Tk()
-    start_window_instance.geometry("535x500")
-    start_window_instance.title("Ventana de Inicio")
-    start_window_instance.resizable(0,0)
-
-    label = tk.Label(start_window_instance, text="Seleccione un símbolo para empezar", font=("Helvetica", 16))
-    label.pack(pady=20)
-
-    selected = tk.StringVar(value="X")
-    x_radio = tk.Radiobutton(start_window_instance, text="X", variable=selected, value="X", font=("Helvetica", 12))
-    x_radio.pack(pady=5)
-    o_radio = tk.Radiobutton(start_window_instance, text="O", variable=selected, value="O", font=("Helvetica", 12))
-    o_radio.pack(pady=5)
-
-    start_button = tk.Button(start_window_instance, text="Comenzar", command=lambda: start_game(selected.get()), font=("Helvetica", 12), bg="yellow")
-    start_button.pack(pady=10)
-
-    start_window_instance.mainloop()
-
-def start_game(selected_option):
-    global player_symbol, computer_symbol, turn
-    if selected_option == "X":
-        player_symbol = "x"
-        computer_symbol = "o"
-    else:
-        player_symbol = "o"
-        computer_symbol = "x"
-    turn = player_symbol
-    start_window_instance.destroy()  
-    game_window()  
-
-def game_window():
-    global root, frame1, frame2, titleLabel, buttons, game_end, turn, player_symbol, computer_symbol, board, player_score, computer_score, empate_score
-
-    root = Tk()
-    root.geometry("535x500")
-    root.title("Tic Tac Toe")
-    root.resizable(0,0)
-
-    frame1 = Frame(root)
-    frame1.pack()
-    titleLabel = Label(frame1 , text="Tic Tac Toe" , font=("Arial" , 26) , bg="orange" , width=27 )
-    titleLabel.grid(row=0 , column=0)
-
-    frame2 = Frame(root , bg="yellow")
-    frame2.pack()
-
-    board = { 1:" " , 2:" " , 3:" ",
-              4:" " , 5:" " , 6:" ",
-              7:" " , 8:" " , 9:" " }
-
-    game_end = False
-    mode = "singlePlayer"
-
-    def updateBoard():
-        for key in board.keys():
-            buttons[key-1]["text"] = board[key]
-
-    def checkForWin(player):
-        # rows
-        if board[1] == board[2] and board[2] == board[3] and board[3] == player:
-            return True
-        
-        elif board[4] == board[5] and board[5] == board[6] and board[6] == player:
-            return True
-        
-        elif board[7] == board[8] and board[8] == board[9] and board[9] == player:
-            return True
-
-        # columns
-        elif board[1] == board[4] and board[4] == board[7] and board[7] == player:
-            return True
-        
-        elif board[2] == board[5] and board[5] == board[8] and board[8] == player:
-            return True
-        
-        elif board[3] == board[6] and board[6] == board[9] and board[9] == player:
-            return True
-        
-        # diagonals
-        elif board[1] == board[5] and board[5] == board[9] and board[9] == player:
-            return True
-        
-        elif board[3] == board[5] and board[5] == board[7] and board[7] == player:
-            return True
-
-        return False
-
-    def resetGame():
-        global game_end, board, turn
-        game_end = False
-        board = {i: " " for i in range(1, 10)}  # Reiniciar el tablero
-        updateBoard()  # Actualizar la interfaz
-        titleLabel.config(text="Tic Tac Toe")
-        turn = player_symbol
-
-    def checkForDraw():
-        for i in board.keys():
-            if board[i] == " ":
-                return False
-        return True
-
-    def minimax(board , isMaximizing):
-        if checkForWin(computer_symbol):
-            return 1 
-        
-        if checkForWin(player_symbol):
-            return -1
-        
-        if checkForDraw():
-            return 0
-        
-        if isMaximizing:
-            bestScore = -100
-            for key in board.keys():
-                if board[key] == " ":
-                    board[key] = computer_symbol
-                    score = minimax(board , False) # minimax
-                    board[key] = " "
-                    if score > bestScore : 
-                        bestScore = score 
-            return bestScore
-        else:
-            bestScore = 100
-            for key in board.keys():
-                if board[key] == " ":
-                    board[key] = player_symbol
-                    score = minimax(board , True) # minimax
-                    board[key] = " "
-                    if score < bestScore : 
-                        bestScore = score 
-            return bestScore
-
-    def playComputer():
-        bestScore = -100
-        bestMove = 0
-        for key in board.keys():
-            if board[key] == " ":
-                board[key] = computer_symbol
-                score = minimax(board , False) # minimax
-                board[key] = " "
-                if score > bestScore : 
-                    bestScore = score 
-                    bestMove = key
-        board[bestMove] = computer_symbol
-        updateBoard()
-
-    def play(event):
-        global turn, game_end, player_score, computer_score, empate_score
-        if game_end:
-            return
-        
-        button = event.widget
-        button_index = buttons.index(button) + 1
-        
-        if button["text"] == " ":
-            board[button_index] = turn
-            updateBoard()
-            if checkForWin(turn):
-                if turn == player_symbol:
-                    player_score += 1
-                else:
-                    computer_score += 1
-                update_score_label()
-                winningLabel = Label(frame1, text=f"{turn} gana el juego", bg="orange", font=("Arial", 26), width=16)
-                winningLabel.grid(row=0, column=0, columnspan=3)
-                game_end = True
-            elif checkForDraw():
-                empate_score += 1
-                update_score_label()
-                drawLabel = Label(frame1, text=f"empate", bg="orange", font=("Arial", 26), width=16)
-                drawLabel.grid(row=0, column=0, columnspan=3)
-                game_end = True
-            else:
-                turn = computer_symbol if turn == player_symbol else player_symbol
-                if mode == "singlePlayer" and turn == computer_symbol and not game_end:
-                    playComputer()
-                    if checkForWin(turn):
-                        computer_score += 1
-                        update_score_label()
-                        winningLabel = Label(frame1, text=f"{turn} gana el juego", bg="orange", font=("Arial", 26), width=16)
-                        winningLabel.grid(row=0, column=0, columnspan=3)
-                        game_end = True
-                    turn = player_symbol
-                    updateBoard()
-    def update_score_label():
-        score_label.config(text=f"Jugador: {player_score} - Computadora: {computer_score} - Empates: {empate_score}")      
-    buttons = []
-    for i in range(3):
-        for j in range(1, 4):
-            button = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="yellow", relief=RAISED, borderwidth=5)
-            button.grid(row=i, column=j)
-            button.bind("<Button-1>", play)
-            buttons.append(button)
-
-    restartButton = Button(frame2 , text="Reiniciar" , width=19 , height=1 , font=("Arial" , 20) , bg="Green" , relief=RAISED , borderwidth=5 , command=resetGame )
-    restartButton.grid(row=4 , column=1 , columnspan=3)
-    score_label = Label(frame1, text=f"Jugador: {player_score} - Computadora: {computer_score} - Empates: {empate_score}", font=("Arial", 16), bg="white")
-    score_label.grid(row=1, column=0)
-    root.mainloop()
+        self.withdraw()  # Oculta la ventana principal
+        options_window = OptionsWindow(self)  # Abre la ventana de opciones
 
 class OptionsWindow(ctk.CTkToplevel):
     def __init__(self, parent):
@@ -273,7 +151,7 @@ class OptionsWindow(ctk.CTkToplevel):
         self.geometry("1080x720")
 
         # Cargar la imagen de fondo de la ventana de opciones
-        self.original_image = Image.open("img2.jpg")  
+        self.original_image = Image.open("./images/img2.jpg")  
         self.background_label = tk.Label(self)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1) 
 
@@ -310,6 +188,10 @@ class OptionsWindow(ctk.CTkToplevel):
     def on_close(self):
         self.destroy()  # Cierra la ventana de opciones
         self.master.deiconify()  # Muestra
+
+if __name__ == "__main__":
+    app = MainMenu()
+    app.mainloop()
 
 if __name__ == "__main__":
     app = MainMenu()
