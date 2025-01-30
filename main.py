57120f31669b7d50090b367aa7a228140cfdaf0f
Daniela <danielaalexandrarangel@gmail.com>
2025-01-30 00:52:07 -0400

    Ajustes a opciones

--- main.py
+++ main.py
@@ -16,18 +16,81 @@ class SelectDifficulty(ctk.CTkFrame):
     def __init__(self, parent, on_difficulty_selected):
         super().__init__(parent, fg_color="transparent")  # Establecer el fondo del frame como transparente
         self.pack(pady=20)
-
-        label = ctk.CTkLabel(self, text="Selecciona la Dificultad", font=("Helvetica", 26), text_color="white")  # Establecer el color del texto como negro
-        label.pack(pady=10)
-
+        
         self.on_difficulty_selected = on_difficulty_selected
 
-        easy_button = ctk.CTkButton(self, text="Fácil", command=lambda: self.set_difficulty("easy"), width=500, height=80, fg_color="transparent", hover_color="#45A049")
-        easy_button.pack(pady=10)
-        medium_button = ctk.CTkButton(self, text="Medio", command=lambda: self.set_difficulty("medium"), width=500, height=80, fg_color="transparent", hover_color="#8E24AA")
-        medium_button.pack(pady=10)
-        hard_button = ctk.CTkButton(self, text="Difícil", command=lambda: self.set_difficulty("hard"), width=500, height=80, fg_color="transparent", hover_color="#E53935")
-        hard_button.pack(pady=10)
+        # Guardar referencias a las imágenes de los botones
+        self.button_images = {}
+
+        # Renderizar los botones con la misma fuente y estilo
+        def render_button_text(text, font_size):
+            button_font = pygame.font.Font('./useful/fuente.ttf', font_size)
+            text_surface = button_font.render(text, True, (255, 255, 255))
+            width, height = text_surface.get_size()
+            button_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+            button_surface.blit(text_surface, (0, 0))
+            pygame.image.save(button_surface, f'{text}_button.png')
+            image = Image.open(f'{text}_button.png')
+            self.button_images[text] = ImageTk.PhotoImage(image)  # Guardar referencia
+            return self.button_images[text]
+
+        self.easy_button = ctk.CTkButton(
+             self,
+             text="",
+             image=render_button_text("FACIL", 20),
+             command=lambda: self.set_difficulty("easy"),
+             width=300,
+             height=80,
+             fg_color="#490029",
+             hover_color="#8B004E",
+             font=("Helvetica", 20),
+             border_color="#6a0000",
+             border_width=3,
+             text_color="white"
+         )
+        self.easy_button.pack(pady=20)
+
+        self.medium_button = ctk.CTkButton(
+             self,
+             text="",
+             image=render_button_text("MEDIO", 20),
+             command=lambda: self.set_difficulty("medium"),
+             width=300,
+             height=80,
+             fg_color="#490029",
+             hover_color="#8B004E",
+             font=("Helvetica", 20),
+             border_color="#6a0000",
+             border_width=3,
+             text_color="white"
+         )
+        self.medium_button.pack(pady=20)
+
+        self.hard_button = ctk.CTkButton(
+             self,
+             text="",
+             image=render_button_text("DIFICIL", 20),
+             command=lambda: self.set_difficulty("hard"),
+             width=300,
+             height=80,
+             fg_color="#490029",
+             hover_color="#8B004E",
+             font=("Helvetica", 20),
+             border_color="#6a0000",
+             border_width=3,
+             text_color="white"
+         )
+        self.hard_button.pack(pady=20)
+
+        # label = ctk.CTkLabel(self, text="Selecciona la Dificultad", font=("Helvetica", 26), text_color="white")  # Establecer el color del texto como negro
+        # label.pack(pady=10)
+
+        # easy_button = ctk.CTkButton(self, text="Fácil", command=lambda: self.set_difficulty("easy"), width=500, height=80, fg_color="transparent", hover_color="#45A049")
+        # easy_button.pack(pady=10)
+        # medium_button = ctk.CTkButton(self, text="Medio", command=lambda: self.set_difficulty("medium"), width=500, height=80, fg_color="transparent", hover_color="#8E24AA")
+        # medium_button.pack(pady=10)
+        # hard_button = ctk.CTkButton(self, text="Difícil", command=lambda: self.set_difficulty("hard"), width=500, height=80, fg_color="transparent", hover_color="#E53935")
+        # hard_button.pack(pady=10)
 
     def set_difficulty(self, selected_difficulty):
         self.on_difficulty_selected(selected_difficulty)
@@ -44,17 +107,6 @@ class SelectSymbol(ctk.CTkToplevel):
 
         # Eliminar los botones de minimizar y maximizar
         self.overrideredirect(True)
-        
-        # Cargar la imagen de fondo del menú
-        try:
-            self.original_image = Image.open("./images/fondo_menu.gif")
-            self.frames = [ImageTk.PhotoImage(img.copy().resize((1280, 720), Image.LANCZOS)) for img in ImageSequence.Iterator(self.original_image)]
-        except Exception as e:
-            print(f"Error al cargar la imagen: {e}")
-            self.frames = []
-
-        self.background_label = tk.Label(self)
-        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
         
         # Renderizar el texto del título con la misma fuente y estilo
         arcade_font = pygame.font.Font('./useful/fuente.ttf', 30)
@@ -88,7 +140,7 @@ class SelectSymbol(ctk.CTkToplevel):
             button_surface.blit(text_surface, (0, 0))
             pygame.image.save(button_surface, f'{text}_button.png')
             image = Image.open(f'{text}_button.png')
-            self.button_images[text] = CTkImage(light_image=image, dark_image=image, size=(width, height))  # Guardar referencia
+            self.button_images[text] = ImageTk.PhotoImage(image)  # Guardar referencia
             return self.button_images[text]
 
         if difficulty == "medium":
@@ -108,24 +160,6 @@ class SelectSymbol(ctk.CTkToplevel):
         # # Botón para volver al menú principal
         back_button = ctk.CTkButton(self, text="", image=render_button_text("Volver al Menú Principal", 20), command=self.back_to_main_menu, width=200, height=40, fg_color="#607D8B", hover_color="#546E7A")
         back_button.pack(pady=10)
-        
-    def update_background(self):
-        if self.frames:
-            frame = self.frames[0]
-            self.background_label.configure(image=frame)
-            self.background_label.image = frame  # Mantener una referencia
-        else:
-            print("No se pudo cargar la imagen de fondo.")  # Mensaje de error si la imagen no se carga
-
-    def resize_background(self, event):
-        self.update_background()
-
-    def animate_gif(self, frame_index):
-        if self.frames:
-            frame = self.frames[frame_index]
-            self.background_label.configure(image=frame)
-            self.after(100, self.animate_gif, (frame_index + 1) % len(self.frames))
-
 
     def setup_music(self):
         if not is_muted:
@@ -147,7 +181,6 @@ class SelectSymbol(ctk.CTkToplevel):
     def back_to_main_menu(self):
         self.destroy()
         self.master.deiconify()  # Mostrar la ventana principal
-        
 
 class MainMenu(ctk.CTk):
     def __init__(self):
@@ -351,7 +384,7 @@ class MainMenu(ctk.CTk):
 
     def play(self):
         pygame.mixer.music.stop()  
-        self.withdraw()
+        self.withdraw()  
         self.select_symbol()
 
     def select_symbol(self):
@@ -411,6 +444,9 @@ class OptionsWindow(ctk.CTkToplevel):
         self.maxsize(1280, 720)
         self.iconbitmap("./images/icono_juego.ico")
         self.attributes('-toolwindow', True)  # Deshabilitar minimizar y maximizar
+        self.is_muted = is_muted
+        
+        self.overrideredirect(True)
 
         # Cargar la imagen de fondo de la ventana de opciones
         try:
@@ -423,66 +459,95 @@ class OptionsWindow(ctk.CTkToplevel):
         self.background_label = tk.Label(self)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        # Redimensionar la imagen al tamaño de la ventana
         self.bind("<Configure>", self.resize_background)
 
         self.update_background()
-        self.animate_gif(0)
+        
+        # Renderizar el texto del título con la misma fuente y estilo
+        arcade_font = pygame.font.Font('./useful/fuente.ttf', 30)
+        text_surface = arcade_font.render('DIFICULTAD', True, (255, 255, 255))
+        shadow_surface = arcade_font.render('DIFICULTAD', True, (0, 0, 0))
+        width, height = text_surface.get_size()
+        border_surface = pygame.Surface((width + 20, height + 20), pygame.SRCALPHA)
+        border_surface.fill((0, 0, 0, 0))
+        border_surface.blit(shadow_surface, (10, 10))
+        border_surface.blit(text_surface, (5, 5))
+        pygame.image.save(border_surface, 'temp_text.png')
+        image = Image.open('temp_text.png')
+        photo = ImageTk.PhotoImage(image)
+        
+        label = tk.Label(self, image=photo, bg="#490029", bd=5, relief="ridge")
+        label.image = photo  # Guardar una referencia de la imagen
+        label.pack(pady=10)
 
-        self.options_label = ctk.CTkLabel(self, text="Opciones del Juego", font=("Arial", 50))
-        self.options_label.pack(pady=20)
+        # Guardar referencias a las imágenes de los botones
+        self.button_images = {}
+        
+        # Renderizar los botones con la misma fuente y estilo
+        def render_button_text(text, font_size):
+            button_font = pygame.font.Font('./useful/fuente.ttf', font_size)
+            text_surface = button_font.render(text, True, (255, 255, 255))
+            width, height = text_surface.get_size()
+            button_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+            button_surface.blit(text_surface, (0, 0))
+            pygame.image.save(button_surface, f'{text}_button.png')
+            image = Image.open(f'{text}_button.png')
+            self.button_images[text] = ImageTk.PhotoImage(image)  # Guardar referencia
+            return self.button_images[text]
 
         self.difficulty_frame = SelectDifficulty(self, self.on_difficulty_selected)
         self.difficulty_frame.pack(pady=20)
-
-        self.back_button = ctk.CTkButton(self, text="VOLVER", command=self.on_back, width=300, height=80, fg_color="#0b5345", hover_color="lightgreen", font=("Arial", 20))
-        self.back_button.pack(pady=20)
-
-        # Botón de silenciar
-        self.mute_button = ctk.CTkButton(self, text="", command=self.toggle_mute, width=300, height=80, fg_color="#0b5345", hover_color="lightgreen", font=("Arial", 20))
-        self.mute_button.pack(pady=20)
         
-    
-    def update_background(self):
-        if self.frames:
-            frame = self.frames[0]
-            self.background_label.configure(image=frame)
-            self.background_label.image = frame  # Mantener una referencia
-        else:
-            print("No se pudo cargar la imagen de fondo.")  # Mensaje de error si la imagen no se carga
-
-    def resize_background(self, event):
-        self.update_background()
-
-    def animate_gif(self, frame_index):
-        if self.frames:
-            frame = self.frames[frame_index]
-            self.background_label.configure(image=frame)
-            self.after(100, self.animate_gif, (frame_index + 1) % len(self.frames))
-
-        # Inicializar el texto del botón según el estado de silencio
-        self.update_mute_button_text()
+        self.mute_button = ctk.CTkButton(
+        self,
+        text="",
+        image=render_button_text("SILENCIAR MUSICA", 20),
+        command=self.toggle_mute,
+        width=300,
+        height=80,
+        fg_color="#490029",
+        hover_color="#8B004E",
+        font=("Helvetica", 20),
+        border_color="#6a0000",
+        border_width=3,
+        text_color="white"
+        )
+        self.mute_button.pack(pady=20)   
+        
+        # Agregar el botón de "Volver al Menú"
+        self.back_button = ctk.CTkButton(
+            self,
+            text="",
+            image=render_button_text("VOLVER", 20),
+            command=self.on_back,
+            width=300,
+            height=80,
+            fg_color="#490029",
+            hover_color="#8B004E",
+            font=("Helvetica", 20),
+            border_color="#6a0000",
+            border_width=3,
+            text_color="white"
+        )
+        self.back_button.pack(pady=20)
 
         # Manejar el evento de cierre de la ventana
         self.protocol("WM_DELETE_WINDOW", self.close_window)
 
-    def update_mute_button_text(self):
-        if is_muted:
-            self.mute_button.configure(text="ACTIVAR MÚSICA")
+    def setup_music(self):
+        if not is_muted:
+            pygame.mixer.music.load("./music/Uma Thurman 8 Bit.mp3")
+            pygame.mixer.music.play(-1)
+            pygame.mixer.music.set_volume(0.4)
         else:
-            self.mute_button.configure(text="SILENCIAR MÚSICA")
+            pygame.mixer.music.set_volume(0)  # Silenciar música si is_muted es True
 
     def toggle_mute(self):
-        global is_muted 
-        is_muted = not is_muted  
-        if is_muted:
+        self.is_muted = not self.is_muted  # Actualizar el valor de is_muted
+        if self.is_muted:
             pygame.mixer.music.set_volume(0)  # Silenciar música
         else:
             pygame.mixer.music.set_volume(0.4)  # Restaurar volumen
-        self.master.setup_music()  
-        self.update_mute_button_text() 
-
-
 
     def resize_background(self, event):
         self.update_background()
